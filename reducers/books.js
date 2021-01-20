dec2c47587eaa54fdfb8c38791abdf8f41d0da41
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-01-20 20:12:04 +0600

    Add reducer and actions

---
+++ reducers/books.js
@@ -0,0 +1,33 @@
+const initialState = {
+  books: [],
+};
+
+const CREATE_BOOK = 'CREATE_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return {
+        ...state,
+        books: state.books.concat({
+          id: new Date(),
+          title: action.title,
+          category: action.category,
+        }),
+      };
+    case REMOVE_BOOK: {
+      const updatedArray = state.books.filter(
+        book => book.id !== action.bookId,
+      );
+      return {
+        ...state,
+        books: updatedArray,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
