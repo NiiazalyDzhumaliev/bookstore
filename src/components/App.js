a8dea5ce2196a5ea482f5a49ed613eac521e0c25
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-01-28 15:32:59 +0600

    Style the header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import BookList from '../containers/BookList';
 import BooksForm from '../containers/BooksForm';
+import Header from './Header';
 import styles from './App.module.css';
 
 function App() {
   return (
     <div className={styles.app}>
       <div className={styles.panel_main}>
+        <Header />
         <BookList />
         <BooksForm />
       </div>
