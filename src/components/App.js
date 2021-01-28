996bd1896e2d88f5d323de0cf1c1089138c85413
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-01-28 14:03:26 +0600

    Add main panel

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,10 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <BookList />
-      <BooksForm />
+      <div className={styles.panel_main}>
+        <BookList />
+        <BooksForm />
+      </div>
     </div>
   );
 }
